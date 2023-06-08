a785ae2b68d32e53fc69968ba6a347860bc6da2f
giovana.borges <gigi12345as17@gmail.com>
2023-06-08 19:25:23 -0300

    :feat add oracle connection to database

--- src/main/java/br/com/fiapchallenge/infra/ConnectionFactory.java
+++ src/main/java/br/com/fiapchallenge/infra/ConnectionFactory.java
@@ -5,15 +5,39 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+	private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+	private static final String USERNAME = "RM96547";
+	private static final String PASSWORD = "210603";
 
-	private ConnectionFactory(){}
-
-	public static Connection getConnection() {
+	public static void main(String[] args) {
 		try {
-			return DriverManager.getConnection("jdbc:mysql://localhost/TesteDB", "root", "123");
-		} catch (SQLException ex) {
-			throw new RuntimeException(ex);
+			conectarBancoDados();
+			System.out.println("Conexão estabelecida com sucesso!");
+		} catch (SQLException e) {
+			System.err.println("Não foi possível conectar ao banco de dados.");
+			exibirMensagemErro(e);
 		}
+	}
+
+	private static Connection conectarBancoDados() throws SQLException {
+		Connection conexao = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		return conexao;
+	}
+
+	private static void exibirMensagemErro(SQLException e) {
+		System.err.println("Mensagem de erro: " + e.getMessage());
+	}
+
+	public static Connection getConnection() throws SQLException {
+		try {
+			return conectarBancoDados();
+
+		} catch (SQLException e) {
+			System.err.println("Não foi possível conectar ao banco de dados.");
+			exibirMensagemErro(e);
+		}
+
+		return conectarBancoDados();
 	}
 
 	
